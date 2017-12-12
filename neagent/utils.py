cdd245f5dbb59e1051e2b5b5b5b8915906dc82f9
Igor Nemilentsev <trezorg@gmail.com>
2017-12-12 14:35:19 +0300

    Fixes

--- neagent/utils.py
+++ neagent/utils.py
@@ -1,3 +1,4 @@
+"""Utils."""
 from colorama import Fore
 
 __all__ = (
@@ -7,10 +8,12 @@ __all__ = (
 
 
 def print_color_line(text, color, new_line=True):
+    """Print function."""
     message = '{0}{1}{2}'.format(color, text, Fore.RESET)
     print(message, **({} if new_line else {'end': ' '}))
 
 
 def prepare_dsn(db_name):
+    """Help Dsn function."""
     return ('Driver=SQLite3;SERVER=localhost;Database={};'
             'Trusted_connection=yes').format(db_name)
